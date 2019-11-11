5dc53b9bac1433c1eb4ddbd5a418683e7f41d0c7
tjr2020 <tjr2020@F1092-32-2120>
2019-11-11 17:17:13 -0600

    tic tac toe in JS

--- TicTacToe/wwwroot/js/site.js
+++ TicTacToe/wwwroot/js/site.js
@@ -2,3 +2,60 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+
+var currentTurn = "X";
+
+document.getElementById("turn").innerText = "It is player " + currentTurn + "'s turn";
+
+var squares = document.getElementsByClassName("square");
+for (var i = 0; i < squares.length; i++)
+{
+    squares[i].addEventListener('click', onclick);
+}
+
+function onClick(event) {
+    event.preventDefault();
+    if (!event.target.innerHTML) {
+        event.target.innerHTML = currentTurn;
+        nextTurn();
+        if (!checkForWin()) {
+            nextTurn();
+        }
+    }
+}
+
+function nextTurn() {
+    if (currentTurn === 'X') {
+        currentTurn = 'O';
+    }
+    else {
+        currentTurn = 'X';
+    }
+}
+
+function declareWinner() {
+    document.getElementById("turn").innerText = "Player " + currentTurn + " wins!";   
+}
+
+function checkForWin() {
+    //Check for rows
+    for (var i = 0; i < 9; i += 3) {
+        if (squares[i].innerText && squares[i].innerText === squares[i + 1].innerText && squares[i + 1].innerText === squares[i + 2].innerText) {
+            declareWinner();
+            return true;
+        }
+    }
+    //Check for columns
+    for (var j = 0; j < 9; j += 1) {
+        if (squares[i].innerText && squares[j].innerText === squares[j + 3].innerText && squares[j + 3].innerText == squares[j + 6].innerText) {
+            declareWinner();
+            return true;
+        }
+    }
+    //Check diagonals
+
+    for (var z = 0; z < 9; z++) {
+        if (squares[z].innerText && squares[z].innerText === squares[z + 4].innerText && squares[z + 2].innerText == squares[z + 5].innerText)
+    }
+}
+    
